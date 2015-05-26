37d54a21786ac4464aba4bac2cfdfe4ddec597f5
Vincent Guillou <vin.guillou@gmail.com>
2015-05-27 01:59:43 +0200

    <fullscreen-api> integration, new style tweaks and page-a content

--- polymer-sample-webapp/serviceworker.js
+++ polymer-sample-webapp/serviceworker.js
@@ -2,7 +2,7 @@ importScripts('serviceworker-cache-polyfill.js');
 
 // Cache version - To update everytime something cached resources
 // changes in order to clear up old cache and build new one.
-var CACHE_VERSION = 7;
+var CACHE_VERSION = 8;
 
 // Shorthand identifier mapped to specific versioned cache.
 var CURRENT_CACHES = {
