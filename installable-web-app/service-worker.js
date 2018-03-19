4cfc99e04afb1a18144dcb2ee2a4d1395a1924ff
Vincent Guillou <vguillou@users.noreply.github.com>
2018-03-19 14:31:59 +0100

    Update service-worker.js

--- installable-web-app/service-worker.js
+++ installable-web-app/service-worker.js
@@ -1,7 +1,46 @@
 'use strict';
 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.html',
+  'about.html'
+];
+
+// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
+});
+
+self.addEventListener('fetch', function(event) {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      // Respond from the cache if possible
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        
+        // Otherwise do the actual fetch and cache the response
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
