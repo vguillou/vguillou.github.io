af97da0f0163e840bfe5e3dfe36dcea8795a735c
Vincent Guillou <vin.guillou@gmail.com>
2020-01-05 01:15:15 +0100

    installable-web-app: Fix service worker

--- installable-web-app/service-worker.js
+++ installable-web-app/service-worker.js
@@ -13,21 +13,18 @@ self.addEventListener('fetch', function(event) {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      // Respond from the cache if possible
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        
-        // Otherwise do the actual fetch and cache the response
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+      
+      // Do the actual fetch and cache the response
+      caches.open(RUNTIME).then(cache => {
+        return fetch(event.request).then(response => {
+          // Put a copy of the response in the runtime cache.
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
           });
-        });
+        })
+
+        // If network problem, respond from the cache if possible
+        .catch(() => caches.match(event.request));
       })
     );
   }
